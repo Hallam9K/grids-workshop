6622b1ac72bd9daf3cb7a82f22aacb4739bf70e6
Hallam9K <hallam@codepagesystems.com>
2026-01-06 16:26:50 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-const canvasSize = 720;
+const canvasSize = 1000;
 const numberOfCells = 40;
 const numberOfBoxes = 20;
 
@@ -8,12 +8,12 @@ const boxSize = canvasSize / numberOfCells;
 let speed = 10;
 
 function setup() {
-  createCanvas(canvasSize - 22, canvasSize - 22);
+  createCanvas(canvasSize, canvasSize);
 
   // Create boxes
-  for (let i = 0; i < 20; i++) {
-    box.push({ x: 0, y: 0 });
-    boxVelocity.push({ x: 10, y: 0 });
+  for (let i = 0; i < 200; i++) {
+    box.push({ x: canvasSize / 2, y: canvasSize / 2 });
+    boxVelocity.push({ x: 0, y: 0 });
   }
 
   fill(255, 0, 0);
@@ -21,70 +21,92 @@ function setup() {
 
 // If off the canvas
 function outOfBounds(num = 0) {
-  if (box[num].x >= 720 - 22 - boxSize)
+  if (box[num].x >= canvasSize - boxSize)
     return 1; // Left
   else if (box[num].x < 0)
     return 2; // Right
-  else if (box[num].y >= 720 - 22 - boxSize)
+  else if (box[num].y >= canvasSize - boxSize)
     return 3; // Down
   else if (box[num].y < 0)
     return 4; // Up
   return 0;
 }
 
-let timer = 0
+let timer = 0;
+let cooldown = 0;
 function moveBoxes() {
+  timer++;
   for (let i = 0; i < box.length; i++) {
-
-    let moveIntervals = boxSize + 50;
+    let moveIntervals = boxSize * 4;
 
     switch (outOfBounds(i)) {
       default:
+        if (cooldown-- >= 0)
+          break;
 
-        let changeDir = random([0, 1, 2]) == 0 ? true : false;
+        let changeDir = random([0, 1, 2]);
         // Randomly change direction
-        if (box[i].x % moveIntervals == 0 && changeDir) {
+        if (box[i].x % (Math.round(box[i].x / moveIntervals) * moveIntervals) == 0 && changeDir == 1) {
           let moveDir = random([0, 1]) == 0 ? true : false;
           if (moveDir) {
             boxVelocity[i].x = -speed;
             boxVelocity[i].y = 0;
+
+            box[i].y = Math.round(box[i].y / moveIntervals) * moveIntervals;
+
           }
           else {
             boxVelocity[i].x = speed;
             boxVelocity[i].y = 0;
+
+            box[i].y = Math.round(box[i].y / moveIntervals) * moveIntervals;
           }
+          cooldown = 50;
         }
-        if (box[i].y % moveIntervals == 0 && changeDir) {
-          let moveDir = random([0, 1]) == 1 ? true : false;
+        if (box[i].y % (Math.round(box[i].y / moveIntervals) * moveIntervals) == 0 && changeDir == 2) {
+          let moveDir = random([0, 1]) == 0 ? true : false;
           if (moveDir) {
             boxVelocity[i].x = 0;
             boxVelocity[i].y = -speed;
+
+            box[i].x = Math.round(box[i].x / moveIntervals) * moveIntervals;
           }
           else {
             boxVelocity[i].x = 0;
             boxVelocity[i].y = speed;
+
+            box[i].x = Math.round(box[i].x / moveIntervals) * moveIntervals;
           }
+          cooldown = 50;
         }
         break;
       // If Out of Bounds
       case 1:
         boxVelocity[i].x = -speed;
         boxVelocity[i].y = 0;
+
+        box[i].y = Math.round(box[i].y / moveIntervals) * moveIntervals;
         break;
       case 2:
         boxVelocity[i].x = speed;
         boxVelocity[i].y = 0;
+
+        box[i].y = Math.round(box[i].y / moveIntervals) * moveIntervals;
         break;
       case 3:
         boxVelocity[i].y = -speed;
         boxVelocity[i].x = 0;
+
+        box[i].x = Math.round(box[i].x / moveIntervals) * moveIntervals;
         break;
       case 4:
         boxVelocity[i].y = speed;
         boxVelocity[i].x = 0;
+
+        box[i].x = Math.round(box[i].x / moveIntervals) * moveIntervals;
         break;
     }
-    if (timer++ >= 60) {
+    if (timer >= 30) {
       box[i].x += boxVelocity[i].x;
       box[i].y += boxVelocity[i].y;
     }
@@ -96,17 +118,19 @@ function draw() {
   strokeWeight(0);
 
   // Make Strips
-  fill(200, 100, 100);
-  for (let i = 0; i < boxSize; i++) {
+  fill(100, 0, 0);
+  for (let i = 0; i < boxSize + 1; i++) {
+    if (i % 2 != 0)
+      continue;
     // Vertical Rows
-    rect(i * (boxSize) + (i * 50), 0, boxSize, canvasSize);
+    rect(0 + (i * boxSize) * 2, 0, boxSize, canvasSize);
     // Horizontal Rows
-    rect(0, i * (boxSize) + (i * 50), canvasSize, boxSize);
+    rect(0, 0 + (i * boxSize) * 2, canvasSize, boxSize);
   }
 
   moveBoxes();
 
-  fill(0, 200, 200);
+  fill(0, 255, 255);
   for (let i = 0; i < box.length; i++)
     square(box[i].x, box[i].y, boxSize);
 }
